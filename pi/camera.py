4781c9b1d507747db1c761537f6b0d0f1f21a3d8
RecycleBot Raspberry Pi <pi@raspberrypi.local>
2019-11-15 10:52:19 -0800

    finish camera

--- pi/camera.py
+++ pi/camera.py
@@ -20,17 +20,5 @@ class CameraController(object):
         output = io.BytesIO()
         self.camera.capture(output, format='bmp')
         output.seek(0)
-        return Image.open(output)
+        return output.getvalue()
 
-
-if __name__ == '__main__':
-    camera = CameraController()
-
-    start_time = time.time()
-    for i in range(5):
-        pic = camera.take_picture()
-    end_time = time.time()
-    pic.save('image.bmp')
-
-    print('Average time', (end_time - start_time)/5)
-    camera.close()
